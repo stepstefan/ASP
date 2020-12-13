1adf5af5f6642b7086841d4dc9809ea6906ccac9
stepstefan <stefanstepanovic@gmail.com>
2020-12-13 18:55:15 +0100

    Added prim's and dijkstra

--- Domaci2/dz1.cpp
+++ Domaci2/dz1.cpp
@@ -1,26 +1,48 @@
 //
-// graph.cc
+// dz1.cpp
 //
 // Created by Stefan Stepanovic on 12/12/2020
 
 #include <iostream>
 #include <limits>
 #include <vector>
-#include <priority_queue>
+#include <utility>
+#include <map>
 
+const size_t ERROR_ID = std::numeric_limits<size_t>::infinity();
+
+/**
+ * @brief Undirected graph structure 
+ * 
+ */
 typedef struct graph
 {
+    // keep track of connected nodes
     std::vector<std::vector<bool>> connectivity_;
+
+    // keep track of weights of edges
+    // data on specific edge is valid only if connectivity of those nodes is true
     std::vector<std::vector<double>> data_;
-    int n = 0;
+
+    std::map<size_t, size_t> node_index_map_;
+
+    // number of nodes
+    size_t n = 0;
+
+    size_t max_index = 0;
 } Graph;
 
 
+/**
+ * @brief Free memory occupied by graph
+ * 
+ * @param graph 
+ */
 void freeGraph(Graph* graph)
 {
     if (graph->n > 0)
     {
-        for (int i = 0; i < graph->n; i++)
+        for (size_t i = 0; i < graph->n; i++)
         {
             graph->data_[i].clear();
             graph->data_[i].shrink_to_fit();
@@ -31,22 +53,41 @@ void freeGraph(Graph* graph)
         graph->data_.shrink_to_fit();
         graph->connectivity_.clear();
         graph->connectivity_.shrink_to_fit();
+        graph->node_index_map_.clear();
     }
     graph->n = 0;
 }
 
-void initializeGraph(Graph* graph, int n)
+/**
+ * @brief Initialize graph with n unconnected nodes
+ * 
+ * @param graph 
+ * @param n 
+ */
+void initializeGraph(Graph* graph, size_t n)
 {
     freeGraph(graph);
     graph->connectivity_ = std::vector<std::vector<bool>>(n, std::vector<bool>(n, false));
     graph->data_ = std::vector<std::vector<double>>(n, std::vector<double>(n, std::numeric_limits<double>::infinity()));
+    for (size_t i = 0; i < n; i++)
+    {
+        graph->node_index_map_.insert(std::make_pair(i, i));
+    }
     graph->n = n;
+    graph->max_index = n-1;
 }
 
-void addNode(Graph* graph)
+/**
+ * @brief Add node to graph and assign it next successive id
+ * 
+ * @param graph 
+ */
+size_t addNode(Graph* graph)
 {
     if (graph->n > 0)
     {
+        size_t new_id = ++graph->max_index;  // find current max id and increment it
+
         graph->data_.push_back(std::vector<double>(graph->n, std::numeric_limits<double>::infinity()));
         graph->connectivity_.push_back(std::vector<bool>(graph->n, false));
 
@@ -55,30 +96,45 @@ void addNode(Graph* graph)
             graph->data_[i].push_back(std::numeric_limits<double>::infinity());
             graph->connectivity_[i].push_back(false);
         }
+
+        graph->node_index_map_.insert(std::make_pair(new_id, graph->n));  // assign mapping from id to index
         graph->n++;
+        return new_id;
     }
     else
     {
         printf("Graph is not initialized!\n");
+        return ERROR_ID;
     }
 }
 
-void addEdge(Graph* graph, int node1, int node2, double weight = 0)
+/**
+ * @brief Add edge between nodes with specified weight
+ * 
+ * @param graph 
+ * @param node1 
+ * @param node2 
+ * @param weight 
+ */
+void addEdge(Graph* graph, size_t node1, size_t node2, double weight = 0)
 {
     if (graph->n > 0)
     {
-        if ((node1 < graph->n) && (node2 < graph->n) &&
-            (node1 >= 0) && (node2 > 0))
+        if ((graph->node_index_map_.find(node1) != graph->node_index_map_.end()) &&
+            (graph->node_index_map_.find(node2) != graph->node_index_map_.end()))
         {
-            graph->connectivity_[node1][node2] = true;
-            graph->connectivity_[node2][node1] = true;
-            graph->data_[node1][node2] = weight;
-            graph->data_[node2][node1] = weight;
-            printf("Added edge between %d and %d\n", node1, node2);
+            size_t idx1 = graph->node_index_map_[node1];
+            size_t idx2 = graph->node_index_map_[node2];
+
+            graph->connectivity_[idx1][idx2] = true;
+            graph->connectivity_[idx2][idx1] = true;
+            graph->data_[idx1][idx2] = weight;
+            graph->data_[idx2][idx1] = weight;
+            printf("Added edge between %ld and %ld\n", node1, node2);
         }
         else
         {
-            printf("Provided nodes are out of bouds %d %d\n", node1, node2);
+            printf("Provided nodes do not exist: %ld %ld\n", node1, node2);
         }
     }
     else
@@ -87,21 +143,31 @@ void addEdge(Graph* graph, int node1, int node2, double weight = 0)
     }
 }
 
-void removeEdge(Graph* graph, int node1, int node2)
+/**
+ * @brief Remove edge between nodes
+ * 
+ * @param graph 
+ * @param node1 
+ * @param node2 
+ */
+void removeEdge(Graph* graph, size_t node1, size_t node2)
 {
     if (graph->n > 0)
     {
-        if ((node1 < graph->n) && (node2 < graph->n) &&
-            (node1 >= 0) && (node2 > 0))
+        if ((graph->node_index_map_.find(node1) != graph->node_index_map_.end()) &&
+            (graph->node_index_map_.find(node2) != graph->node_index_map_.end()))
         {
-            graph->connectivity_[node1][node2] = false;
-            graph->connectivity_[node2][node1] = false;
-            graph->data_[node1][node2] = std::numeric_limits<double>::infinity();
-            graph->data_[node2][node1] = std::numeric_limits<double>::infinity();
+            size_t idx1 = graph->node_index_map_[node1];
+            size_t idx2 = graph->node_index_map_[node2];
+
+            graph->connectivity_[idx1][idx2] = false;
+            graph->connectivity_[idx2][idx1] = false;
+            graph->data_[idx1][idx2] = std::numeric_limits<double>::infinity();
+            graph->data_[idx2][idx1] = std::numeric_limits<double>::infinity();
         }
         else
         {
-            printf("Provided nodes are aout of bouds %d %d", node1, node2);
+            printf("Provided nodes do not exist: %ld %ld", node1, node2);
         }
     }
     else
@@ -110,46 +176,75 @@ void removeEdge(Graph* graph, int node1, int node2)
     }
 }
 
-void removeNode(Graph* graph, int node)
+/**
+ * @brief Remove specific node from graph
+ * 
+ * @param graph 
+ * @param node 
+ */
+void removeNode(Graph* graph, size_t node)
 {
-    if (graph->n > 1)
+    if (graph->node_index_map_.find(node) != graph->node_index_map_.end())
     {
-        graph->connectivity_.erase(graph->connectivity_.begin() + node);
-        graph->data_.erase(graph->data_.begin() + node);
+        size_t idx = graph->node_index_map_[node];
+        graph->connectivity_.erase(graph->connectivity_.begin() + idx);
+        graph->data_.erase(graph->data_.begin() + idx);
 
         for (size_t i = 0; i < graph->n - 1; i++)
         {
-            graph->connectivity_.at(i).erase(graph->connectivity_.at(i).begin() + node);
-            graph->data_.at(i).erase(graph->data_.at(i).begin() + node);
+            graph->connectivity_.at(i).erase(graph->connectivity_.at(i).begin() + idx);
+            graph->data_.at(i).erase(graph->data_.at(i).begin() + idx);
         }
-
+        for (auto& element : graph->node_index_map_)
+        {
+            if (element.second > idx)
+            {
+                element.second--;
+            }
+        }
+        graph->node_index_map_.erase(node);
         graph->n--;
     }
     else
     {
-        printf("Graph must have more than one node!\n");
+        printf("Node does not exist!\n");
     }
 }
 
+/**
+ * @brief Print graph to std::out
+ * 
+ * @param graph 
+ */
 void printGraph(Graph* graph)
 {
-    printf("Number of nodes %d\n", graph->n);
-    for (int i = 0; i < graph->n; i++)
+    printf("Number of nodes %ld\n", graph->n);
+    std::cout << "   ";
+    for (const auto& element : graph->node_index_map_)
     {
-        for (int j = 0; j < graph->n; j++)
-        {
-            std::cout << graph->connectivity_.at(i).at(j) << " ";
-        }
-        std::cout << std::endl;
+        std::cout << element.first << "\t";
     }
 
-    for (int i = 0; i < graph->n; i++)
+    std::cout << std::endl;
+    std::cout << "   ";
+    for (const auto& element : graph->node_index_map_)
     {
-        for (int j = 0; j < graph->n; j++)
+        std::cout << "_\t";
+    }
+
+    std::cout << std::endl;
+
+    for (const auto& element1 : graph->node_index_map_)
+    {
+        std::cout << element1.first << "| ";
+        for (const auto& element2 : graph->node_index_map_)
         {
-            std::cout << graph->data_.at(i).at(j) << " ";
+            size_t idx1 = element1.second;
+            size_t idx2 = element2.second;
+
+            std::cout << graph->data_.at(idx1).at(idx2) << "\t";
         }
-        std::cout << std::endl;;
+        std::cout << std::endl;
     }
 }
 
@@ -175,17 +270,17 @@ int main()
     int sw;
 
     printhelp();
-    printf("\nEnter yout choice: ");
+    printf("\nEnter your choice: ");
 
     scanf("%d", &sw);
     while (sw)
     {
-        int n, node, node1, node2;
+        size_t n, node, node1, node2;
         switch (sw)
         {
             case (1):
                 printf("Insert number of nodes: ");
-                scanf("%d", &n);
+                scanf("%ld", &n);
                 initializeGraph(&graph, n);
                 break;
             case (2):
@@ -193,17 +288,17 @@ int main()
                 break;
             case (3):
                 printf("Select node: ");
-                scanf("%d", &node);
+                scanf("%ld", &node);
                 removeNode(&graph, node);
                 break;
             case (4):
                 printf("Select nodes: ");
-                scanf("%d %d", &node1, &node2);
+                scanf("%ld %ld", &node1, &node2);
                 addEdge(&graph, node1, node2);
                 break;
             case (5):
                 printf("Select nodes: ");
-                scanf("%d %d", &node1, &node2);
+                scanf("%ld %ld", &node1, &node2);
                 removeEdge(&graph, node1, node2);
                 break;
             case (6):
@@ -215,7 +310,7 @@ int main()
                 printhelp();
                 break;
         }
-        printf("\nEnter yout choice: ");
+        printf("\nEnter your choice: ");
         scanf("%d", &sw);
     }
 
--- Domaci2/dz2.cpp
+++ Domaci2/dz2.cpp
@@ -1,28 +1,53 @@
 //
-// graph.cc
+// dz2.cpp
 //
 // Created by Stefan Stepanovic on 12/12/2020
 
-#include <stdio.h>
 #include <iostream>
-#include <stdlib.h>
-#include <stdbool.h>
 #include <limits>
 #include <vector>
+#include <utility>
+#include <map>
+#include <cstdlib>
+#include <ctime>
+#include <queue>
+#include <set>
+#include <stack>
 
+const size_t ERROR_ID = std::numeric_limits<size_t>::infinity();
+
+/**
+ * @brief Undirected graph structure 
+ * 
+ */
 typedef struct graph
 {
+    // keep track of connected nodes
     std::vector<std::vector<bool>> connectivity_;
+
+    // keep track of weights of edges
+    // data on specific edge is valid only if connectivity of those nodes is true
     std::vector<std::vector<double>> data_;
-    int n = 0;
+
+    std::map<size_t, size_t> node_index_map_;
+
+    // number of nodes
+    size_t n = 0;
+
+    size_t max_index = 0;
 } Graph;
 
 
+/**
+ * @brief Free memory occupied by graph
+ * 
+ * @param graph 
+ */
 void freeGraph(Graph* graph)
 {
     if (graph->n > 0)
     {
-        for (int i = 0; i < graph->n; i++)
+        for (size_t i = 0; i < graph->n; i++)
         {
             graph->data_[i].clear();
             graph->data_[i].shrink_to_fit();
@@ -33,22 +58,41 @@ void freeGraph(Graph* graph)
         graph->data_.shrink_to_fit();
         graph->connectivity_.clear();
         graph->connectivity_.shrink_to_fit();
+        graph->node_index_map_.clear();
     }
     graph->n = 0;
 }
 
-void initializeGraph(Graph* graph, int n)
+/**
+ * @brief Initialize graph with n unconnected nodes
+ * 
+ * @param graph 
+ * @param n 
+ */
+void initializeGraph(Graph* graph, size_t n)
 {
     freeGraph(graph);
     graph->connectivity_ = std::vector<std::vector<bool>>(n, std::vector<bool>(n, false));
     graph->data_ = std::vector<std::vector<double>>(n, std::vector<double>(n, std::numeric_limits<double>::infinity()));
+    for (size_t i = 0; i < n; i++)
+    {
+        graph->node_index_map_.insert(std::make_pair(i, i));
+    }
     graph->n = n;
+    graph->max_index = n-1;
 }
 
-void addNode(Graph* graph)
+/**
+ * @brief Add node to graph and assign it next successive id
+ * 
+ * @param graph 
+ */
+size_t addNode(Graph* graph)
 {
     if (graph->n > 0)
     {
+        size_t new_id = ++graph->max_index;  // find current max id and increment it
+
         graph->data_.push_back(std::vector<double>(graph->n, std::numeric_limits<double>::infinity()));
         graph->connectivity_.push_back(std::vector<bool>(graph->n, false));
 
@@ -57,30 +101,45 @@ void addNode(Graph* graph)
             graph->data_[i].push_back(std::numeric_limits<double>::infinity());
             graph->connectivity_[i].push_back(false);
         }
+
+        graph->node_index_map_.insert(std::make_pair(new_id, graph->n));  // assign mapping from id to index
         graph->n++;
+        return new_id;
     }
     else
     {
         printf("Graph is not initialized!\n");
+        return ERROR_ID;
     }
 }
 
-void addEdge(Graph* graph, int node1, int node2, double weight = 0)
+/**
+ * @brief Add edge between nodes with specified weight
+ * 
+ * @param graph 
+ * @param node1 
+ * @param node2 
+ * @param weight 
+ */
+void addEdge(Graph* graph, size_t node1, size_t node2, double weight = 0)
 {
     if (graph->n > 0)
     {
-        if ((node1 < graph->n) && (node2 < graph->n) &&
-            (node1 >= 0) && (node2 > 0))
+        if ((graph->node_index_map_.find(node1) != graph->node_index_map_.end()) &&
+            (graph->node_index_map_.find(node2) != graph->node_index_map_.end()))
         {
-            graph->connectivity_[node1][node2] = true;
-            graph->connectivity_[node2][node1] = true;
-            graph->data_[node1][node2] = weight;
-            graph->data_[node2][node1] = weight;
-            printf("Added edge between %d and %d\n", node1, node2);
+            size_t idx1 = graph->node_index_map_[node1];
+            size_t idx2 = graph->node_index_map_[node2];
+
+            graph->connectivity_[idx1][idx2] = true;
+            graph->connectivity_[idx2][idx1] = true;
+            graph->data_[idx1][idx2] = weight;
+            graph->data_[idx2][idx1] = weight;
+            printf("Added edge between %ld and %ld\n", node1, node2);
         }
         else
         {
-            printf("Provided nodes are out of bouds %d %d\n", node1, node2);
+            printf("Provided nodes do not exist: %ld %ld\n", node1, node2);
         }
     }
     else
@@ -89,21 +148,31 @@ void addEdge(Graph* graph, int node1, int node2, double weight = 0)
     }
 }
 
-void removeEdge(Graph* graph, int node1, int node2)
+/**
+ * @brief Remove edge between nodes
+ * 
+ * @param graph 
+ * @param node1 
+ * @param node2 
+ */
+void removeEdge(Graph* graph, size_t node1, size_t node2)
 {
     if (graph->n > 0)
     {
-        if ((node1 < graph->n) && (node2 < graph->n) &&
-            (node1 >= 0) && (node2 > 0))
+        if ((graph->node_index_map_.find(node1) != graph->node_index_map_.end()) &&
+            (graph->node_index_map_.find(node2) != graph->node_index_map_.end()))
         {
-            graph->connectivity_[node1][node2] = false;
-            graph->connectivity_[node2][node1] = false;
-            graph->data_[node1][node2] = std::numeric_limits<double>::infinity();
-            graph->data_[node2][node1] = std::numeric_limits<double>::infinity();
+            size_t idx1 = graph->node_index_map_[node1];
+            size_t idx2 = graph->node_index_map_[node2];
+
+            graph->connectivity_[idx1][idx2] = false;
+            graph->connectivity_[idx2][idx1] = false;
+            graph->data_[idx1][idx2] = std::numeric_limits<double>::infinity();
+            graph->data_[idx2][idx1] = std::numeric_limits<double>::infinity();
         }
         else
         {
-            printf("Provided nodes are aout of bouds %d %d", node1, node2);
+            printf("Provided nodes do not exist: %ld %ld", node1, node2);
         }
     }
     else
@@ -112,114 +181,264 @@ void removeEdge(Graph* graph, int node1, int node2)
     }
 }
 
-void removeNode(Graph* graph, int node)
+/**
+ * @brief Remove specific node from graph
+ * 
+ * @param graph 
+ * @param node 
+ */
+void removeNode(Graph* graph, size_t node)
 {
-    if (graph->n > 1)
+    if (graph->node_index_map_.find(node) != graph->node_index_map_.end())
     {
-        graph->connectivity_.erase(graph->connectivity_.begin() + node);
-        graph->data_.erase(graph->data_.begin() + node);
+        size_t idx = graph->node_index_map_[node];
+        graph->connectivity_.erase(graph->connectivity_.begin() + idx);
+        graph->data_.erase(graph->data_.begin() + idx);
 
         for (size_t i = 0; i < graph->n - 1; i++)
         {
-            graph->connectivity_.at(i).erase(graph->connectivity_.at(i).begin() + node);
-            graph->data_.at(i).erase(graph->data_.at(i).begin() + node);
+            graph->connectivity_.at(i).erase(graph->connectivity_.at(i).begin() + idx);
+            graph->data_.at(i).erase(graph->data_.at(i).begin() + idx);
         }
-
+        for (auto& element : graph->node_index_map_)
+        {
+            if (element.second > idx)
+            {
+                element.second--;
+            }
+        }
+        graph->node_index_map_.erase(node);
         graph->n--;
     }
     else
     {
-        printf("Graph must have more than one node!\n");
+        printf("Node does not exist!\n");
     }
 }
 
+/**
+ * @brief Print graph to std::out
+ * 
+ * @param graph 
+ */
 void printGraph(Graph* graph)
 {
-    printf("Number of nodes %d\n", graph->n);
-    for (int i = 0; i < graph->n; i++)
+    printf("Number of nodes %ld\n", graph->n);
+    std::cout << "   ";
+    for (const auto& element : graph->node_index_map_)
     {
-        for (int j = 0; j < graph->n; j++)
+        std::cout << element.first << "\t";
+    }
+
+    std::cout << std::endl;
+    std::cout << "   ";
+    for (const auto& element : graph->node_index_map_)
+    {
+        std::cout << "_\t";
+    }
+
+    std::cout << std::endl;
+
+    for (const auto& element1 : graph->node_index_map_)
+    {
+        std::cout << element1.first << "| ";
+        for (const auto& element2 : graph->node_index_map_)
         {
-            std::cout << graph->connectivity_.at(i).at(j) << " ";
+            size_t idx1 = element1.second;
+            size_t idx2 = element2.second;
+
+            std::cout << graph->data_.at(idx1).at(idx2) << "\t";
         }
         std::cout << std::endl;
     }
-
-    for (int i = 0; i < graph->n; i++)
-    {
-        for (int j = 0; j < graph->n; j++)
-        {
-            std::cout << graph->data_.at(i).at(j) << " ";
-        }
-        std::cout << std::endl;;
-    }
 }
 
+typedef std::pair<double, size_t> EdgeType;  // weight, node: default comparison of std::pair type takes first element into consideration
 
-void printhelp()
+double Prims(Graph* graph)
 {
-    printf("0 : exit\n");
-    printf("1 : initialize graph\n");
-    printf("2 : add node\n");
-    printf("3 : remove node\n");
-    printf("4 : add edge\n");
-    printf("5 : remove edge\n");
-    printf("6 : print graph\n");
-    printf("7 : free graph\n");
-    printf("100 : print help\n");
+    srand((unsigned) time(0));
+    int randomNumber = (std::rand() % graph->n);
+    std::map<size_t, size_t>::iterator it = graph->node_index_map_.begin();
+    std::advance(it, randomNumber);
+
+    size_t start_node = 0;
+
+    std::cout << "Node: " << start_node << std::endl;
+
+
+    std::priority_queue<EdgeType, std::vector<EdgeType>, std::greater<EdgeType>> pqueue;  // min p_queue
+    std::map<size_t, bool> added;
+    for (const auto& element : graph->node_index_map_)
+    {
+        added.insert(std::make_pair(element.first, false));
+    }
+
+    pqueue.push(std::make_pair(0, start_node));
+
+    double total_cost = 0.0;
+
+    while (!pqueue.empty())
+    {
+        EdgeType edge = pqueue.top();
+        pqueue.pop();
+
+        double weight = edge.first;
+        size_t node = edge.second;
+        size_t index = graph->node_index_map_[node];
+
+        if (!added[node])
+        {
+            total_cost += weight;
+            added[node] = true;
+
+            std::cout << "Node: " << node << std::endl;
+
+
+            for (const auto& element : graph->node_index_map_)
+            {
+                if (graph->connectivity_[index][element.second])
+                {
+                    if (!added[element.first])
+                    {
+                        pqueue.push(std::make_pair(graph->data_[index][element.second], element.first));
+                    }
+                }
+            }
+        }
+    }
+    return total_cost;
+}
+
+void MinPaths(Graph* graph, size_t start_node)
+{
+    std::priority_queue<EdgeType, std::vector<EdgeType>, std::greater<EdgeType>> pqueue;  // min p_queue
+
+    // Set distances to inf
+    std::map<size_t, double> distances;
+    for (const auto& element : graph->node_index_map_)
+    {
+        distances.insert(std::make_pair(element.first, std::numeric_limits<double>::infinity()));
+    }
+
+    // keeps track of previous nodes for every node
+    std::map<size_t, std::vector<std::vector<size_t>>> track;
+    for (const auto& element : graph->node_index_map_)
+    {
+        track.insert(std::make_pair(element.first, std::vector<std::vector<size_t>>()));
+    }
+
+    std::vector<std::vector<size_t>> empty_track;
+    empty_track.push_back(std::vector<size_t>());
+    track.at(start_node) = empty_track;
+
+    std::map<size_t, bool> finished;
+    for (const auto& element : graph->node_index_map_)
+    {
+        finished.insert(std::make_pair(element.first, false));
+    }
+    finished.at(start_node) = true;
+
+    // Init values for start node
+    pqueue.push(std::make_pair(0, start_node));
+    distances.at(start_node) = 0;
+
+    while (!pqueue.empty())
+    {
+        EdgeType edge = pqueue.top();
+        pqueue.pop();
+
+        size_t node = edge.second;
+        std::cout << "Processing: " << node << std::endl;
+        double weight = edge.first;
+        size_t index = graph->node_index_map_[node];
+        finished.at(node) = true;
+        for (const auto& element : graph->node_index_map_)
+        {
+            if (graph->connectivity_[index][element.second] && !finished.at(element.first))
+            {
+                if (distances[element.first] > distances[node] + graph->data_[index][element.second])
+                {
+                    distances[element.first] = distances[node] + graph->data_[index][element.second];
+                    track[element.first].clear();
+                    track[element.first] = track[node];
+                    for (auto& track_element : track[element.first])
+                    {
+                        track_element.push_back(node);
+                    }
+                    pqueue.push(std::make_pair(distances[element.first], element.first));
+                }
+                else
+                {
+                    if (distances[element.first] == distances[node] + graph->data_[index][element.second])
+                    {
+                        for (const auto& track_element : track[node])
+                        {
+                            std::vector<size_t> copy_track = track_element;
+                            copy_track.push_back(node);
+                            track.at(element.first).push_back(copy_track);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for (const auto element : track)
+    {
+        std::cout << element.first << " track size: " << element.second.size();
+        std::cout << std::endl;
+    }
+
+    for (const auto& element : distances)
+    {
+        std::cout << "Distance " << start_node << " - " << element.first << " = " << element.second << " with paths: \n" << std::flush;
+        for (const auto& track_element : track.at(element.first))
+        {
+            for (const auto& prev_node : track_element)
+            {
+                std::cout << prev_node << " ";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+    }
 }
 
 int main()
 {
     Graph graph;
-    graph.n = 0;
 
-    int sw;
+    size_t size;
+    printf("\nEnter the number of keypoints: ");
 
-    printhelp();
-    printf("\nEnter yout choice: ");
+    scanf("%ld", &size);
+    initializeGraph(&graph, size);
+    printf("\nEnter possible pairs of connected keypoints with possible price for it separated by space");
+    printf("\nFor stop signal insert negative price\n");
 
-    scanf("%d", &sw);
-    while (sw)
+    size_t node1, node2;
+    double price = 0;
+    while (price >= 0)
     {
-        int n, node, node1, node2;
-        switch (sw)
+        scanf("%ld %ld %lf", &node1, &node2, &price);
+        if (price >= 0)
         {
-            case (1):
-                printf("Insert number of nodes: ");
-                scanf("%d", &n);
-                initializeGraph(&graph, n);
-                break;
-            case (2):
-                addNode(&graph);
-                break;
-            case (3):
-                printf("Select node: ");
-                scanf("%d", &node);
-                removeNode(&graph, node);
-                break;
-            case (4):
-                printf("Select nodes: ");
-                scanf("%d %d", &node1, &node2);
-                addEdge(&graph, node1, node2);
-                break;
-            case (5):
-                printf("Select nodes: ");
-                scanf("%d %d", &node1, &node2);
-                removeEdge(&graph, node1, node2);
-                break;
-            case (6):
-                printGraph(&graph);
-                break;
-            case (7):
-                freeGraph(&graph);
-            default:
-                printhelp();
-                break;
+            addEdge(&graph, node1, node2, price);
         }
-        printf("\nEnter yout choice: ");
-        scanf("%d", &sw);
     }
+
+    double cost = Prims(&graph);
+    std::cout << "Optimal cost is: " << cost << std::endl;
+
+    size_t vNode;
+    printf("Enter the V node: ");
+    scanf("%ld", &vNode);
+
+    MinPaths(&graph, vNode);
+
+
+    printGraph(&graph);
 
     freeGraph(&graph);
     return 0;
